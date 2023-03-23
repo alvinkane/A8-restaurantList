55c84afebcaa7fc956bf5542fdb66663b1bcd577
alvin <ckuvn1854@gmail.com>
2023-03-23 21:58:52 +0800

    feat: add mongodb connection

--- app.js
+++ app.js
@@ -6,6 +6,31 @@ const exphbs = require("express-handlebars");
 // 設定參數
 const port = 3000;
 
+// 連線資料庫
+const mongoose = require("mongoose");
+
+// 僅在非正式環境使用dotenv
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+// 設定mongoose連線至mongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+// 取得連線狀態
+const db = mongoose.connection;
+// 異常偵測
+db.on("error", () => {
+  console.log("mongodb error");
+});
+// 成功偵測
+db.once("open", () => {
+  console.log("mongodb connected");
+});
+
 // 設定靜態網站
 app.use(express.static("public"));
 // 導入餐廳清單
