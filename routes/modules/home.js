4501c491160bb653a91e93e31b53920fece7266b
alvin <ckuvn1854@gmail.com>
2023-03-25 22:34:48 +0800

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,8 +4,12 @@ const router = express.Router();
 const Restaurant = require("../../models/restaurant");
 // 根目錄
 router.get("/", (req, res) => {
+  // 默認用name排序
+  const index = req.query.sortBy || "name";
+  const order = Number(req.query.sortOrder) === 1 ? "asc" : "desc";
   Restaurant.find()
     .lean()
+    .sort({ [index]: order })
     .then((restaurants) => {
       res.render("index", { restaurants });
     })
