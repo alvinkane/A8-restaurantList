3e7336dfc36553da77c17e2d01d3308b8a3935d5
alvin <ckuvn1854@gmail.com>
2023-03-26 18:15:34 +0800

    feat: edit and new hbs add datalist in category

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,35 +1,49 @@
 // 載入model
 const Restaurant = require("../restaurant");
 // 載入json
-const restaurantList = require("../../restaurant.json");
+const restaurantList = require("../../restaurant.json").results;
 
 const db = require("../../config/mongoose");
 
-db.once("open", () => {
-  restaurantList.results.forEach((restaurant) => {
-    const {
-      name,
-      name_en,
-      category,
-      image,
-      location,
-      phone,
-      google_map,
-      rating,
-      description,
-    } = restaurant;
-    Restaurant.create({
-      name,
-      name_en,
-      category,
-      image,
-      location,
-      phone,
-      google_map,
-      rating,
-      description,
-    });
-  });
+//第一種作法
+// db.once("open", () => {
+//   restaurantList.results.forEach((restaurant) => {
+//     const {
+//       name,
+//       name_en,
+//       category,
+//       image,
+//       location,
+//       phone,
+//       google_map,
+//       rating,
+//       description,
+//     } = restaurant;
+//     Restaurant.create({
+//       name,
+//       name_en,
+//       category,
+//       image,
+//       location,
+//       phone,
+//       google_map,
+//       rating,
+//       description,
+//     });
+//   });
 
-  console.log("done");
+//   console.log("done");
+// });
+
+//第二種作法
+db.once("open", () => {
+  console.log("running restaurantSeeder script...");
+
+  Restaurant.create(restaurantList)
+    .then(() => {
+      console.log("restaurantSeeder done!");
+      // 終端機不用再自己停止
+      db.close();
+    })
+    .catch((err) => console.log(err));
 });
