8d8ce4d1eaf63004edfcd36cad3c818514480d0c
alvin <ckuvn1854@gmail.com>
2023-03-26 09:08:03 +0800

    fix: search function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -18,4 +18,22 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/search", (req, res) => {
+  const keyword = req.query.keyword;
+  // 使用regular expression，"i"可以忽略大小寫
+  const regex = new RegExp(keyword, "i");
+  Restaurant.find({
+    // 使用or在三個領域尋找
+    $or: [{ name: regex }, { name_en: regex }, { category: regex }],
+  })
+    .lean()
+    .then((restaurants) => {
+      res.render("index", { restaurants });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
 module.exports = router;
+// , name_en: regex, category: regex
